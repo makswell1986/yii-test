88925715cac4d8b4d0f3bcfa62a8bb7bb4485f7d
maksim1986 <maxfergana1986@gmail.com>
2024-08-11 23:31:21 +0500

    11082024

--- config/web.php
+++ config/web.php
@@ -8,6 +8,7 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'layout'=>'site',
+    'name'=>'Oxel',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
--- views/site/index.php
+++ views/site/index.php
@@ -2,52 +2,441 @@
 
 /** @var yii\web\View $this */
 
-$this->title = 'My Yii Application';
+$this->title = Yii::$app->name;
 ?>
-<div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
+    <section class=" slider_section position-relative">
+      <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
+          <div class="col-md-2 offset-md-2">
+            <div class="slider_heading">
+              <h2>
+                Bo <span>xer</span>
+              </h2>
             </div>
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
+          </div>
+          <div class="col-md-8 mx-auto">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+              <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">01</li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1">02</li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2">03</li>
+              </ol>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div class="box">
+                    <div class="detail-box">
+                      <h1>
+                        Boxing <br />
+                        Center
+                      </h1>
+                      <hr />
+                      <div class="btn-box">
+                        <a href="" class="btn-1">
+                          Contact Us
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div class="box">
+                    <div class="detail-box">
+                      <h1>
+                        Boxing <br />
+                        Center
+                      </h1>
+                      <hr />
+                      <div class="btn-box">
+                        <a href="" class="btn-1">
+                          Contact Us
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div class="box">
+                    <div class="detail-box">
+                      <h1>
+                        Boxing <br />
+                        Center
+                      </h1>
+                      <hr />
+                      <div class="btn-box">
+                        <a href="" class="btn-1">
+                          Contact Us
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="sr-only">Previous</span>
+              </a>
+              <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="sr-only">Next</span>
+              </a>
             </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+          </div>
         </div>
+      </div>
 
+    </section>
+    <!-- end slider section -->
+  </div>
+
+  <!-- about section -->
+
+  <section class="about_section layout_padding">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-10 ml-auto pr-0">
+          <div class="about_container">
+            <div class="row">
+              <div class="col-lg-3 col-md-5">
+                <div class="detail-box">
+                  <div class="heading_container">
+                    <h2>
+                      About Class
+                    </h2>
+                  </div>
+                  <p>
+                    iusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris iusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquipnisi ut aliquipiusmod tempor incididunt ut labore et
+                  </p>
+                  <hr />
+                  <a href="">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-</div>
+  </section>
+  <!-- end about section -->
+
+  <!-- class section -->
+
+  <section class="class_section ">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-10 col-lg-8 mx-auto">
+          <div class="class_container">
+            <div class="row">
+              <div class="col-lg-9 col-md-10">
+                <div class="heading_container">
+                  <h2>
+                    Our Classes Videos
+                  </h2>
+                  <p>
+                    iusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis n
+                    ostrud exercitation ullamco laboris iusmod tempor incididunt ut labore et dolore
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="class_box-container">
+              <div class="owl-carousel owl_carousel1">
+                <div class="item">
+                  <div class="box">
+                    <div class="img-box">
+                      <img src="images/c1.jpg" alt="">
+                      <div class="detail-box">
+                        <button>
+                          <img src="images/play-icon.png" alt="">
+                        </button>
+                        <h2>
+                          Boxing
+                        </h2>
+                      </div>
+                    </div>
+                    <div class="btn-box">
+                      <a href="">
+                        See More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="box">
+                    <div class="img-box">
+                      <img src="images/c2.jpg" alt="">
+                      <div class="detail-box">
+                        <button>
+                          <img src="images/play-icon.png" alt="">
+                        </button>
+                        <h2>
+                          Boxing
+                        </h2>
+                      </div>
+                    </div>
+                    <div class="btn-box">
+                      <a href="">
+                        See More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="box">
+                    <div class="img-box">
+                      <img src="images/c1.jpg" alt="">
+                      <div class="detail-box">
+                        <button>
+                          <img src="images/play-icon.png" alt="">
+                        </button>
+                        <h2>
+                          Boxing
+                        </h2>
+                      </div>
+                    </div>
+                    <div class="btn-box">
+                      <a href="">
+                        See More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div class="item">
+                  <div class="box">
+                    <div class="img-box">
+                      <img src="images/c2.jpg" alt="">
+                      <div class="detail-box">
+                        <button>
+                          <img src="images/play-icon.png" alt="">
+                        </button>
+                        <h2>
+                          Boxing
+                        </h2>
+                      </div>
+                    </div>
+                    <div class="btn-box">
+                      <a href="">
+                        See More
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end class section -->
+
+
+  <!-- blog section -->
+
+  <section class="blog_section layout_padding">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-10 ml-auto">
+          <div class="heading_container">
+            <h2>
+              Latest Blog
+            </h2>
+            <p>
+              iusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis n
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 pl-0">
+          <div class="box b1">
+            <div class="img-box">
+              <img src="images/b1.jpg" alt="">
+            </div>
+            <div class="row">
+              <div class="col-lg-8 col-md-10 ml-auto">
+                <div class="detail-box">
+                  <div class="img_date">
+                    <h6>
+                      17 <br>
+                      Feb
+                    </h6>
+                  </div>
+                  <h3>
+                    Boxer Joniya Daro
+                  </h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                  </p>
+                  <a href="">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 pr-0">
+          <div class="box b2">
+            <div class="img-box">
+              <img src="images/b2.jpg" alt="">
+            </div>
+            <div class="row">
+              <div class="col-lg-8 col-md-10 mr-auto">
+                <div class="detail-box">
+                  <div class="img_date">
+                    <h6>
+                      17 <br>
+                      Jun
+                    </h6>
+                  </div>
+                  <h3>
+                    Boxer Joniya Daro
+                  </h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+                  </p>
+                  <a href="">
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end blog section -->
+
+  <!-- client section -->
+
+  <section class="client_section layout_padding-bottom">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-10 col-lg-8 mx-auto">
+          <div class="client_container">
+            <div class="heading_container">
+              <h2>
+                What Says Our students
+              </h2>
+              <p>
+                iusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis n
+              </p>
+            </div>
+            <div class="client_box-container">
+              <div class="carousel-wrap ">
+                <div class="owl-carousel">
+                  <div class="item">
+                    <div class="box">
+                      <div class="img-box">
+                        <img src="images/client1.png" alt="" />
+                      </div>
+                      <div class="detail-box">
+                        <h4>
+                          Jone Moe
+                        </h4>
+                        <p>
+                          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum </p>
+                        <img src="images/quote.png" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <div class="box">
+                      <div class="img-box">
+                        <img src="images/client2.png" alt="" />
+                      </div>
+                      <div class="detail-box">
+                        <h4>
+                          Lio Uji
+                        </h4>
+                        <p>
+                          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum </p>
+                        <img src="images/quote.png" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <div class="box">
+                      <div class="img-box">
+                        <img src="images/client1.png" alt="" />
+                      </div>
+                      <div class="detail-box">
+                        <h4>
+                          Jone Moe
+                        </h4>
+                        <p>
+                          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum </p>
+                        <img src="images/quote.png" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div class="item">
+                    <div class="box">
+                      <div class="img-box">
+                        <img src="images/client2.png" alt="" />
+                      </div>
+                      <div class="detail-box">
+                        <h4>
+                          Lio Uji
+                        </h4>
+                        <p>
+                          It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum </p>
+                        <img src="images/quote.png" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end client section -->
+
+  <!-- info section -->
+  <div class="info_section">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-10 ml-auto">
+          <div class="row info_main-row">
+            <div class="col-md-6 pr-0">
+
+              <!-- contact section -->
+
+              <section class="contact_section">
+                <h2>
+                  Request A Call Back
+                </h2>
+                <form action="">
+                  <div>
+                    <input type="text" placeholder="Name" />
+                  </div>
+                  <div>
+                    <input type="text" placeholder="Phone Number" />
+                  </div>
+                  <div>
+                    <input type="email" placeholder="Email" />
+                  </div>
+                  <div>
+                    <input type="text" class="message-box" placeholder="Message" />
+                  </div>
+                  <div class="d-flex ">
+                    <button>
+                      SEND
+                    </button>
+                  </div>
+                </form>
+                <div class="map_container">
+                  <div class="map">
+                    <div id="googleMap" style="width:100%;height:300px;"></div>
+                  </div>
+                </div>
+              </section>
+
+              <!-- end contact section -->
